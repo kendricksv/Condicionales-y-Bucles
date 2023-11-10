c105152570a0f5e5f4da0243e2556b7e254e7f6a
kendricksv <150397910+kendricksv@users.noreply.github.com>
2023-11-10 04:54:41 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int  principal () {
+    int  numero1 , numero2 ;
+    printf ( "Introduce el primer numero: " );
+    scanf ( "%d" & numero 1 ) ;
+    printf ( "Introduce el segundo numero: " );
+    scanf ( "%d" & numero 2 ) ;
+    if ( numero1  >  numero2 ) {
+        printf ( "El numero %d es mayor que el numero %d\n" , numero1 , numero2 );
+    } else if ( numero1  <  numero2 ) {
+        printf ( "El numero %d es mayor que el numero %d\n" , numero2 , numero1 );
+    } else {
+        printf ( "Los numeros son iguales\n" );
+    }
+    devolver  0 ;
+}
